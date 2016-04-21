6d151758f097f31bf3b1c6e9677ff5729b04a451
anderson06 <andersoncampos.m@gmail.com>
2016-04-21 15:08:31 -0300

    style fixes

--- gulpfile.js
+++ gulpfile.js
@@ -75,7 +75,7 @@ gulp.task('jade-build', function() {
 });
 */
 gulp.task('sass', function() {
-	return gulp.src('src/sass/main.scss')
+	return gulp.src('src/sass/*.scss')
 		.pipe(sass({ style: 'expanded' }))
 		.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
 		.pipe(gulp.dest('dist/assets/css'))
